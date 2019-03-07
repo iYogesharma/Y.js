8af849023f2f4f0cc58dada1d0508aca224761c4
Yogesh Sharma <yogesh.sharma1@holisollogistics.com>
2019-03-07 14:12:13 +0530

    cleanup

--- index.js
+++ index.js
@@ -8,7 +8,87 @@
             this[i]=el[i]
         }
         this.length = el.length;
+        
     }
+    function get(selector) {
+        var el;
+        if (typeof selector === "string") {
+            el = document.querySelectorAll(selector);
+        } else if (selector.length) {
+            el = selector;
+        } else {
+            el = [selector];
+        }
+        return new Dome(el);
+    }
+   
+   
+   
+    function addClass(classes) {
+        var className = "";
+        if (typeof classes !== "string") {
+            for (var i = 0; i < classes.length; i++) {
+                className += " " + classes[i];
+            }
+        } else {
+            className = " " + classes;
+        }
+        return this.forEach(function (el) {
+            el.className += className;
+        });
+    };
+    function removeClass(classes) {
+        return this.forEach(function (el) {
+
+            var cs = el.className.split(" "), i;
+     
+            while ( (i = cs.indexOf(classes)) > -1) { 
+                cs = cs.slice(0, i).concat(cs.slice(++i));
+            }
+            el.className = cs.join(" ");
+        }); 
+    };
+    function attr(attr, val) {
+        if (typeof val !== "undefined") {
+            return this.forEach(function(el) {
+                el.setAttribute(attr, val);
+            });
+        } else {
+            return this.mapOne(function (el) {
+                return el.getAttribute(attr);
+            });
+        }
+    };
+    function hide() {
+        return this.forEach(function(el) {
+            el.style.display="none";
+        });
+    };
+    function show() {
+        return this.forEach(function(el) {
+            el.style.display="bloch";
+        });
+    };
+    function text() {
+        return this.singleTag(function(el){
+            if(el.tagName==="INPUT")
+            return el.value
+            return el.innerText;
+        });
+    };
+    function html() {
+        return this.singleTag(function(el){
+            return el.innerHTML;
+        });
+    };
+    function value() {
+        return this.singleTag(function(el){
+            if(el.tagName!="INPUT")
+            return el.innerText
+            return el.value;
+        });
+    };
+    
     dome.prototype.get=function (selector) {
         var el;
         if (typeof selector === "string") {
@@ -20,96 +100,38 @@
         }
         return new Dome(el);
     }
-    Dome.prototype.map = function (callback) {
-        var results = [], i = 0;result=[];
-        for ( ; i < this.length; i++) {
-            results.push(callback.call(this, this[i], i));
-        }
-        return results;
-    };
-    Dome.prototype.mapOne = function (callback) {
-        var m = this.map(callback);
-        return m.length > 1 ? m : m[0];
-    };
-    Dome.prototype.forEach=function (callback) {
-        this.map(callback);
-        return this;
-    };
-    Dome.prototype. addClass =function (classes) {
-        var className = "";
-        if (typeof classes !== "string") {
-            for (var i = 0; i < classes.length; i++) {
-                className += " " + classes[i];
+    Dome.prototype={
+        constructor:Dome, 
+        forEach: function forEach(callback) {
+            this.map(callback);
+            return this;
+        },
+        map: function map(callback) {
+            var results = [], i = 0;result=[];
+            for ( ; i < this.length; i++) {
+                results.push(callback.call(this, this[i], i));
             }
-        } else {
-            className = " " + classes;
-        }
-        return this.forEach(function (el) {
-            // console.log(el)
-            el.className += className;
-        });
-    };
-  
-    Dome.prototype. removeClass =function (classes) {
-        return this.forEach(function (el) {
-
-            var cs = el.className.split(" "), i;
-     
-            while ( (i = cs.indexOf(classes)) > -1) { 
-                cs = cs.slice(0, i).concat(cs.slice(++i));
+            return results;
+        },
+        mapOne: function mapOne(callback) {
+            var m = this.map(callback);
+            return m.length > 1 ? m : m[0];
+        },
+        singleTag: function singleTag(callback) {
+            if(this.length>1){
+                return new Error('Method cannot be called on Node or NodeList');
             }
-            el.className = cs.join(" ");
-        }); 
+            return callback.call(this,this[0],0);
+        },
+        addClass:addClass,
+        removeClass:removeClass,
+        attr:attr,
+        hide:hide,
+        show:show,
+        text:text,
+        html:html,
+        value:value
     };
-
-    Dome.prototype. attr =function (attr, val) {
-        if (typeof val !== "undefined") {
-            return this.forEach(function(el) {
-                el.setAttribute(attr, val);
-            });
-        } else {
-            return this.mapOne(function (el) {
-                return el.getAttribute(attr);
-            });
-        }
-    };
-    Dome.prototype. hide =function() {
-        return this.forEach(function(el) {
-            el.style.display="none";
-        });
-    };
-    Dome.prototype. show =function () {
-        return this.forEach(function(el) {
-            el.style.display="bloch";
-        });
-    };
-    Dome.prototype.text =function () {
-        return this.singleTag(function(el){
-            if(el.tagName==="INPUT")
-            return el.value
-            return el.innerText;
-        });
-    };
-    Dome.prototype.html =function () {
-        return this.singleTag(function(el){
-            return el.innerHTML;
-        });
-    };
-    Dome.prototype.value =function () {
-        return this.singleTag(function(el){
-            if(el.tagName!="INPUT")
-            return el.innerText
-            return el.value;
-        });
-    };
-    Dome.prototype.singleTag =function (callback) {
-        if(this.length>1){
-            return new Error('Method cannot be called on Node or NodeList');
-        }
-        console.log(this)
-        return callback.call(this,this[0],0);
-    };
-    
     if ( !globals ) {
         window.dome = window.Y = dome;
     }
